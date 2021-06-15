157d5e0e6f4e3a743cb9b464fd016b23f522e4a0
sdavi187 <85182019+sdavi187@users.noreply.github.com>
2021-06-15 11:30:39 -0400

    Module2
    Finished Module2

---
+++ Sams/P1M2SamDavis.py
@@ -0,0 +1,5 @@
+# [ ] create, call and test fishstore() function 
+def fishstore (fish, price):
+    return ("Fish Type: " + fish + " costs $" + price)
+
+print (fishstore (input ("What type of fish?"), input ("How much does it cost?")))
