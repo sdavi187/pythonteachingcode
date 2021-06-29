f0292f2779f80064c1408daece72617b00d27200
sdavi187 <85182019+sdavi187@users.noreply.github.com>
2021-06-29 14:47:47 -0400

    Demo Project 2 Complete
    Webpage with Flask

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/sam/')
+def sam():
+    return render_template("sam.html")
+
 if __name__=="__main__":
     app.run(debug=True)
